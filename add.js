8b1c0702365f77b07aa0994891b7d9c0724c5831
이지훈 <ljihoon0330@gmail.com>
2020-09-01 17:40:12 +0900

    일정 등록 폼 완성

--- add.js
+++ add.js
@@ -1,14 +1,25 @@
-{
-  /* <li>
+/* <li>
   <span>1111111111111111111111111111111111111111111111111111111111111</span>
   <i class="fas fa-trash-alt"></i>
 </li>; */
-}
-const content = document.querySelector(".content");
-const add__form = document.querySelector(".add__form");
-const addButton = document.querySelector(".add");
 
-addButton.addEventListener("click", () => {
+// 12313 2020-12-31 00:59
+
+const content = document.querySelector(".content");
+const form = document.querySelector(".add__form");
+const schedule = document.querySelector(".add__schedule");
+const label = document.querySelector("label");
+// const date = document.querySelector(".add__date");
+// const time = document.querySelector(".add__time");
+const submit = document.querySelector(".add_submit");
+const button = document.querySelector(".add");
+
+button.addEventListener("click", () => {
   content.classList.toggle("hidden");
-  add__form.classList.toggle("hidden");
+  form.classList.toggle("hidden");
+});
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  console.log(``, schedule.value, date.value, time.value);
 });
--- clock.js
+++ clock.js
@@ -1,5 +1,9 @@
+// 12313 2020-12-31 00:59  // "2019/05/16/0:0:10"
+
 const clock = document.querySelector(".clock");
 const ampm = document.querySelector(".ampm");
+const date = document.querySelector(".add__date");
+const time = document.querySelector(".add__time");
 
 const getDateTime = option => {
   const date = new Date();
@@ -10,13 +14,15 @@ const getDateTime = option => {
       minutes: date.getMinutes(),
       seconds: date.getSeconds()
     };
-
+    return dateObj;
+  } else {
+    const dateObj = {
+      year: date.getFullYear(),
+      day: date.getDate(),
+      month: date.getMonth() + 1
+    };
     return dateObj;
   }
-
-  const year = date.getFullYear();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
 };
 
 const getAMPM = hours => (hours >= 12 ? "PM" : "AM");
@@ -32,8 +38,21 @@ const getTime = () => {
     `;
 };
 
+const setDate = () => {
+  const { year, month, day } = getDateTime("date");
+  const { hours, minutes } = getDateTime("time");
+
+  date.value = `${year}-${month < 10 ? `0${month}` : month}-${
+    day < 10 ? `0${day}` : day
+  }`;
+  time.value = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }`;
+};
+
 const init = () => {
   getTime();
+  setDate();
   setInterval(getTime, 1000);
 };
 
